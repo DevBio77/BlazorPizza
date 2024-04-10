d82cce76db01e1c0ed4efc59eedf543b9d7a2612
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2024-04-10 11:53:17 -0300

    Unificando layout em Componente

--- BlazorPizzaMaui/MauiProgram.cs
+++ BlazorPizzaMaui/MauiProgram.cs
@@ -1,5 +1,4 @@
-﻿using BlazorPizzaMaui.Data;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 
 namespace BlazorPizzaMaui
 {
@@ -21,9 +20,6 @@ namespace BlazorPizzaMaui
     		builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
-
-            builder.Services.AddSingleton<WeatherForecastService>();
-
             return builder.Build();
         }
     }
