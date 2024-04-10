8ee9a559e7cb16e08cf3352d04049b89c7527a76
Fabrício M. Damasceno <fabricio.damasceno@compasso.com.br>
2024-04-10 10:20:40 -0300

    Criando projeto Mobile com Blazor Maui

---
+++ BlazorPizzaMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BlazorPizzaMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
